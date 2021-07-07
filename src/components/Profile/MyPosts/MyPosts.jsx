802b5f159444ad310e83181ee6e12103bf9e1af3
Vits-Rimer Anna <urazaeva.anna@gmail.com>
2021-07-07 16:37:00 +0300

    Added the ability to enter text and receive an alert

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,29 @@
 import style from './MyPosts.module.css';
 import Post from './Post/Post';
+import React from "react";
 
 const MyPosts = (props) => {
-
   let postsElements = props.postsData.map((post) => (
     <Post message={post.message} countLikes={post.countLikes} />
   ));
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+      let text = newPostElement.current.value;
+      alert(text);
+  };
 
   return (
     <div className={style.postsBlock}>
       <h3>My post</h3>
       <div>
         <div>
-          <textarea name="post" id="" cols="18" rows="5">
+          <textarea ref={newPostElement} cols="18" rows="5">
             Что у вас нового?
           </textarea>
         </div>
-        <button>Add post</button>
+        <button onClick={ addPost }>Add post</button>
         <button>Remove</button>
       </div>
       <div className={style.posts}>{postsElements}</div>
