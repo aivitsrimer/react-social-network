0884ef1ace74d177c842f22b57c61e2bf272e1da
Vits-Rimer Anna <urazaeva.anna@gmail.com>
2021-09-02 19:49:51 +0300

    Changed profileReducer with copy of state

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -12,22 +12,27 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 countLikes: 0,
             };
-            state.postsData.push(newPost);
-            state.newPostText = '';
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.postsData = [...state.postsData]
+            stateCopy.postsData.push(newPost);
+            stateCopy.newPostText = '';
+            return stateCopy;
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy = {...state};
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
     }
-}
+};
 
 export const addPostActionCreator = () => ({type: ADD_POST});
 export const updateNewPostTextCreator= (text) =>
