64b353cb7ff4822af228b75edc630adada76a3d0
Vits-Rimer Anna <vitsrimer.anna@gmail.com>
2021-09-12 03:49:54 +0700

    wip

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,18 +1,15 @@
 import styles from './user.module.css';
+import axios from "axios";
 
 let Users = (props) => {
+    const defaultPhoto = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/768px-Circle-icons-profile.svg.png';
 
-    if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/768px-Circle-icons-profile.svg.png',
-                followed: true, fullName: 'Anna', status: 'Frontend-developer', location: {city: 'Tomsk', country: 'Russia'} },
-            { id: 2, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/768px-Circle-icons-profile.svg.png',
-                followed: false, fullName: 'Sasha', status: 'Backend-developer', location: {city: 'Kaliningrad', country: 'Russia'} },
-            { id: 3, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/768px-Circle-icons-profile.svg.png',
-                followed: false, fullName: 'Ilya', status: 'I am a designer', location: {city: 'Kiev', country: 'Ukraine'} },
-            { id: 4, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/768px-Circle-icons-profile.svg.png',
-                followed: false, fullName: 'Diana', status: 'I love a cats', location: {city: 'Moscow', country: 'Russia'} },
-        ]);
+    if (!props.users.length) {
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        });
+        return <div>loading..</div>;
     }
 
     return <div>
@@ -20,7 +17,7 @@ let Users = (props) => {
             props.users.map( user => <div key={user.id}>
                 <span>
                     <div>
-                        <img src={user.photoUrl} className={styles.userPhoto} alt="photo"/>
+                        <img src={user?.photos?.small ?? defaultPhoto} className={styles.userPhoto} alt="photo"/>
                     </div>
                     <div>
                         { user.followed
@@ -30,12 +27,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{user.fullName}</div>
+                        <div>{user.name}</div>
                         <div>{user.status}</div>
                     </span>
                     <span>
-                        <div>{user.location.country}</div>
-                        <div>{user.location.city}</div>
+                        <div>{"user.location.country"}</div>
+                        <div>{"user.location.city"}</div>
                     </span>
                 </span>
             </div>)
